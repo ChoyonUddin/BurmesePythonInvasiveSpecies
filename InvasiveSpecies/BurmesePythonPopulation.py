c157a6c7b87dbb784e81be3e2f04010e77d9439d
ChoyonUddin <choyonuddin17@gmail.com>
2024-12-26 01:06:54 -0500

    Initial Analysis Commit

---
+++ InvasiveSpecies/BurmesePythonPopulation.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn as scy
+
+df = pd.read_csv("FL Specimen Management Database Pre-2022PYMO.csv")
+
+years = df['Year'].value_counts().sort_index()
+y = years.values
+x = years.index
+
+
+plt.figure(figsize=(10, 6))
+
+#Line Graph
+plt.plot(x, y, marker=',', linestyle='solid',markersize =7)
+plt.xticks(x, rotation=45)
+plt.xlabel('Year')
+plt.ylabel('Sightings')
+plt.title('Number of Burmese Pythons Documented Each Year')
+plt.show()
+
+#Bar Graph
+plt.bar(years.index, years.values)
+plt.xticks(x, rotation=45)
+plt.xlabel('Year')
+plt.ylabel('Sightings')
+plt.title('Number of Burmese Pythons Documented Each Year')
+plt.show()
